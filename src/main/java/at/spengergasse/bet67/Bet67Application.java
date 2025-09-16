71dcc5d80083140d94bb6060ffaa01d7c717365d
Felix Eidler <felixeidler@gmail.com>
2025-09-16 06:42:04 +0200

    initial commit

---
+++ src/main/java/at/spengergasse/bet67/Bet67Application.java
@@ -0,0 +1,13 @@
+package at.spengergasse.bet67;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bet67Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bet67Application.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/bet67/Bet67ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.bet67;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bet67ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
